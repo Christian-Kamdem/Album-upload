a7b0030e6f5a1ba55ac38a0323febad168e47b2b
Christian Kamdem <christiankamdem2@gmail.com>
2018-05-20 00:12:39 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>
+			Album (cover + songs) upload
+		</title>
+		<link rel="stylesheet" href="css/style.css" media="all" onload="if(media!='all')media='all'">
+	</head>
+	<body>
+	</body>
+</html>
